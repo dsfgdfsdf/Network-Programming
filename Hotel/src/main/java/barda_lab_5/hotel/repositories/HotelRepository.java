897e1208908746f9e062a4b345a3027b0fc5dc96
Mukola <97519442+dsfgdfsdf@users.noreply.github.com>
2024-11-24 02:05:41 +0200

    Delete Hotel directory

--- Hotel/src/main/java/barda_lab_5/hotel/repositories/HotelRepository.java
+++
@@ -1,6 +0,0 @@
-package barda_lab_5.hotel.repositories;
-
-import barda_lab_5.hotel.entities.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, Long> {}
